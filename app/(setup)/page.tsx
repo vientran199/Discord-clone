27d18f1a746cec0faa96cc48537e178b91734641
Trần Văn Viển <68174942+vientran199@users.noreply.github.com>
2025-03-01 00:26:10 +0700

    create server modal UI

--- app/(setup)/page.tsx
+++ app/(setup)/page.tsx
@@ -2,6 +2,7 @@ import { redirect } from "next/navigation";
 
 import { initialProfile } from "@/lib/initial-profile";
 import { db } from "@/lib/db";
+import { InitialModal } from "@/components/modals/initial-modal";
 
 const SetupPage = async () => {
     const profile = await initialProfile();
@@ -20,9 +21,7 @@ const SetupPage = async () => {
         return redirect(`/servers/${server.id}`)
     }
     return (
-        <div>
-            Create a server
-        </div>
+        <InitialModal />
     )
 }
 
--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        domains: [
+            "k5rddzuw5z.ufs.sh" //config to fix invalid src prop on `next/image`
+        ]
+    }
+}
 
 module.exports = nextConfig
