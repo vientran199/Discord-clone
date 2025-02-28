828939b29dadb079fd50840f9c77b129ae80ac8e
Trần Văn Viển <68174942+vientran199@users.noreply.github.com>
2025-03-01 00:41:45 +0700

    create server endpoint api

--- components/modals/initial-modal.tsx
+++ components/modals/initial-modal.tsx
@@ -3,7 +3,9 @@
 import * as z from 'zod';
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form";
-import { useEffect, useState } from 'react';
+import { useEffect, useReducer, useState } from 'react';
+
+import axios from 'axios'
 
 import {
     Dialog,
@@ -25,6 +27,7 @@ import {
 import { Button } from "@/components/ui/button";
 import { Input } from '@/components/ui/input';
 import { FileUpload } from '@/components/file-upload';
+import { useRouter } from 'next/navigation';
 
 const formSchema = z.object({
     name: z.string().min(1, {
@@ -37,6 +40,8 @@ const formSchema = z.object({
 
 export const InitialModal = () => {
     const [isMounted, setIsMounted] = useState(false);
+
+    const router = useRouter();
 
     useEffect(() => {
         setIsMounted(true)
@@ -53,7 +58,15 @@ export const InitialModal = () => {
     const isLoading = form.formState.isSubmitting
 
     const onSubmit = async (values: z.infer<typeof formSchema>) => {
-        console.log(values);
+        try {
+            await axios.post("/api/servers", values)
+
+            form.reset()
+            router.refresh();
+            location.reload();
+        } catch (error) {
+            console.error(error);
+        }
     }
 
     if (!isMounted) {
