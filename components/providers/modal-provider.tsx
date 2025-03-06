f72f845a53186f41aef215c6d6a0abde0db0f03b
Trần Văn Viển <68174942+vientran199@users.noreply.github.com>
2025-03-06 23:17:11 +0700

    server settings

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -2,6 +2,7 @@
 
 import { CreateServerModal } from "@/components/modals/create-server-modal";
 import { InviteModal } from "@/components/modals/invite-modal";
+import { EditServerModal } from "@/components/modals/edit-server-modal";
 
 import { useEffect, useState } from "react";
 
@@ -20,6 +21,7 @@ export const ModalProvider = () => {
         <>
             <CreateServerModal />
             <InviteModal />
+            <EditServerModal />
         </>
     )
 }
