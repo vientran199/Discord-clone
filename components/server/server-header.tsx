52090c3a307e23606741ed1ec0b7374b85915d7d
Trần Văn Viển <68174942+vientran199@users.noreply.github.com>
2025-03-10 19:25:21 +0700

    leave server

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -88,6 +88,7 @@ export const ServerHeader = ({
                 )}
                 {!isAdmin && (
                     <DropdownMenuItem
+                        onClick={() => onOpen('leaveServer', { server: server })}
                         className="text-rose-500 px-3 py-2 text-sm cursor-pointer"
                     >
                         Leave Server
