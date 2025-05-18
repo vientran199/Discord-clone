68cdb6d4b61c980ab373c6659810fd79e3dfcf1a
Trần Văn Viển <68174942+vientran199@users.noreply.github.com>
2025-05-18 22:19:58 +0700

    chat header and responsive for site bar

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,8 +1,52 @@
-const ChannelIdPage = () => {
+import { redirect } from "next/navigation";
+import { auth } from "@clerk/nextjs/server";
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+
+import { ChatHeader } from "@/components/chat/chat-header";
+
+interface ChannelIdPageProps {
+    params: {
+        serverId: string;
+        channelId: string;
+    };
+}
+
+const ChannelIdPage = async ({
+    params
+}: ChannelIdPageProps) => {
+    const profile = await currentProfile();
+
+    if (!profile) {
+        const { redirectToSignIn } = auth()
+
+        return redirectToSignIn();
+    }
+
+    const channel = await db.channel.findUnique({
+        where: {
+            id: params.channelId,
+        }
+    })
+
+    const member = await db.member.findFirst({
+        where: {
+            serverId: params.serverId,
+            profileId: profile.id,
+        }
+    })
+
+    if (!channel || !member) {
+        return redirect(`/`)
+    }
+
     return (
-        <div>
-            <h1>ChannelIdPage</h1>
-            <p>This is the channel page.</p>
+        <div className="bg-white dark:bg-[#313338] flex flex-col h-full">
+            <ChatHeader
+                name={channel.name}
+                serverId={channel.serverId}
+                type="channel"
+            />
         </div>
     );
 }
